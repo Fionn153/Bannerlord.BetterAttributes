74813d7ce880d5bac16dabe810ff18c58f02916c
Fionn153 <117425057+Fionn153@users.noreply.github.com>
2022-11-12 03:36:30 -0500

    Updated to MCMV5 and Bannerlord 1.0.1
    Just recompiled against the new sources and fixed some imports and method names changes in MCMv5

---
+++ SubModule.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using BetterAttributes.Utils;
+using BetterAttributes.Settings;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using BetterAttributes.Custom;
+
+namespace BetterAttributes {
+	public class SubModule : MBSubModuleBase {
+
+		protected override void OnSubModuleLoad() {
+			base.OnSubModuleLoad();
+
+			Harmony h = new Harmony("Bannerlord.Shadow.BetterAttributes");
+
+			//*** Manual patching reference
+			//MethodInfo original = typeof(Hero).GetProperty("AddSkillXp").GetGetMethod();
+			//MethodInfo postfix = typeof(XPPatch).GetMethod("AddSkillXp");
+			//h.Patch(original, postfix: new HarmonyMethod(postfix));
+
+			h.PatchAll();
+		}
+
+		protected override void OnGameStart(Game game, IGameStarter gameStarter) {
+			base.OnGameStart(game, gameStarter);
+
+			if (game.GameType is Campaign) {
+				CampaignGameStarter campaignGameStarter = gameStarter as CampaignGameStarter;
+
+				if (campaignGameStarter != null) {
+					campaignGameStarter.AddModel(new CustomDefaultPartyWageModel());
+				}
+			}
+		}
+
+		protected override void OnBeforeInitialModuleScreenSetAsRoot() {
+			base.OnBeforeInitialModuleScreenSetAsRoot();
+
+			string modName = base.GetType().Assembly.GetName().Name;
+
+			Helper.SetModName(modName);
+			Helper.settings = SettingsManager.Instance;
+		}
+    }
+}
